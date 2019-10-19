55f65ce149882e539c7968850e425b1851e30fe0
Brad <banicholasdesign@gmail.com>
2019-10-19 12:48:26 -0500

    Burger Menu Addition
    Burger menu toggles out on mobile version

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,20 @@
 alert("Hey there friend, Welcome to my website!");
 
 console.log("Hello World")
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+
+    });
+
+    // navLinks.forEach((link, index) => {
+       // link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    // });
+}
+
+navSlide();
